d1c609cd310416b39858d70b4f096593b22c4e43
luijar <luijar@luisatencio.net>
2016-02-15 19:50:26 -0500

    CH02

--- code/src/ch02/ch02.php
+++ code/src/ch02/ch02.php
@@ -20,6 +20,32 @@
 
 	println('Adding 5 to 5: '. $add5(5));
 
+	function apply(callable $operator, $a, $b) {
+		return $operator($a, $b);
+	}
 
+	$add = function ($a, $b) {
+		return $a + $b;
+	};
+
+	$divide = function ($a, $b) {
+		return $a / $b;
+	};
+
+	println(apply($add, 5, 5));
+
+	println(apply($divide, 5, 5));	
 	
+
+	function apply2(callable $operator) {
+		return function($a, $b) use ($operator) {
+			return $operator($a, $b);
+		};
+	}
+
+	println(apply2($add)(5, 5));
+
+	println(apply2($divide)(5, 5));	
+	
+	apply2($divide)(5, 0);
 
