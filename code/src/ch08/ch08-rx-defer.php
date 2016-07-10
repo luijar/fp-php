6a9ab53595cb4ed2ba0da98d38d2303306520090
luijar <luijar@luisatencio.net>
2016-07-09 23:36:14 -0400

    CH08

--- code/src/ch08/ch08-rx-defer.php
+++ code/src/ch08/ch08-rx-defer.php
@@ -10,6 +10,8 @@ require_once 'common.php';
 
 use \Rx\Observable as Observable;
 use \Rx\Observer as Observer;
+use \Model\Account as Account;
+use \Model\User as User;
 
 println('Example 1 - Simple defer');
 Observable::defer(function () {
@@ -39,3 +41,75 @@ Observable::just('FB')
 	->subscribe(new Observer\CallbackObserver(
         function ($value) { echo "Next value: " . asString($value->resource->fields->name) . "\n"; }
     ));
+
+
+/*
+->combineLatest([$source2], function ($value1, $value2) {
+    return "First: {$value1}, Second: {$value2}";
+})
+*/
+
+ println('Buy 10 shares of FB from Luis CHECKING account');
+	
+	function findSavingsAccountObs($userId) {
+		return Observable::just($userId)
+			->skipWhile(function ($input) {
+				return empty($input);		
+			})
+			->map(function ($userId) {
+				return User::find($userId);	
+			})
+			->doOnNext(function (User $user) {
+				printf("Found user: %s \n", $user->getEmail());
+			})
+			->flatMap(function (User $user) {
+				return Observable::fromArray(Account::query('user_id', $user->getId()));
+			})
+			->takeWhile(function (Account $account) {
+				return $account->getType() === 'SAVINGS';
+			})
+			->doOnNext(function (Account $account) {
+				printf("Found savings account. Current balance: %d \n", $account->getBalance());			
+			});
+	}
+
+	function findSharePrice($symbol) {
+		return Observable::just($symbol)
+			->flatMap(function ($symbol) {
+					return Observable::defer(function () use ($symbol) {
+						$ch = curl_init();
+					    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+					    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					    curl_setopt($ch, CURLOPT_URL, "http://finance.yahoo.com/webservice/v1/symbols/{$symbol}/usd/quote?format=json");
+					    $result = curl_exec($ch);
+					    curl_close($ch);
+						return Observable::just($result);
+				});
+			})
+			->map('json_decode')	
+			->map(function ($result) {				
+				return $result->list->resources[0]->resource->fields->price;
+			});
+	}
+
+	findSavingsAccountObs(1)
+		->combineLatest([findSharePrice('FB')])			
+		->doOnNext(function (array $data) {
+			list($account, $pricePerShare) = $data;
+			printf("Found %s acccount and price per share of %d \n", $account->getType(), $pricePerShare);					        
+		})
+		->map(function ($data) {
+			list($account, $pricePerShare) = $data;
+			return $account->withdraw($pricePerShare * 10)->save();			
+		})		
+		->subscribe(new Observer\CallbackObserver(
+		    function ($account) {
+		    	printf("New account balance: %d \n", $account->getBalance());					        
+		    },
+		    function (Exception $ex) {
+		        print 'Error: ' . $ex->getMessage();
+		    },
+		    function () {
+		        print 'Completed!';
+		    }
+	 	)); 
