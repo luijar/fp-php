400f67afadf4e7de447fab9412dcca9fc3afbd8c
luijar <luijar@luisatencio.net>
2016-11-06 12:36:57 -0500

    BFF;2C

--- code/rx/src/aggregator/bff.php
+++ code/rx/src/aggregator/bff.php
@@ -1,19 +1,47 @@
+#!/usr/local/bin/php7
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__. '/../bootstrap.php';
 
-use Illuminate\Database\Capsule\Manager as DB;
-use Rx\{Observable as Observable, Observer as Observer};
+use Rx\{
+  Observable as Observable,
+  Observer as Observer,
+  React\Promise as Promise,
+  Disposable\CallbackDisposable as Subscription
+};
 
-Observable::fromArray([1,2,3])
-	->subscribe(new \Rx\Observer\CallbackObserver(
-	    function ($val) {
-	        echo 'Next: '. $val. PHP_EOL;
-	    },
-	    function (Exception $ex) {
-	        echo 'Error: ', $ex->getMessage(), PHP_EOL;
-	    },
-	    function () {
-	        echo 'Completed', PHP_EOL;
-	    }
- 	));
+function isValidNumber($val) {
+  return !empty($val) && is_numeric($val);
+}
+
+function curl(string $url): string {
+  echo 'Curling!';
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
+}
+
+// use curried tracer that you can turn on and off
+
+function findTotalBalance(int $userId) {
+  return Observable::just($userId)
+     ->filter('isValidNumber')
+     ->flatMap(function () {
+          return Promise::toObservable(Promise::resolved(curl('http://localhost:8001/users')));
+     })
+     ->subscribeCallback(
+        function ($val) {
+            print_r($val);
+        },
+        function (Exception $ex) {
+            echo 'Error: ', $ex->getMessage(), PHP_EOL;
+        },
+        function () {
+            echo 'Completed', PHP_EOL;
+        }
+    );
+}
+
+findTotalBalance(2);
